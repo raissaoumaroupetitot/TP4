e21bee971450db2c23541573b1e61aae338ea362
raissaoumaroupetitot <raissa.oumarou-petitot@polymtl.ca>
2025-03-10 10:58:04 -0400

    feat: modified myscript.py and main.yml

--- myscript.py
+++ myscript.py
@@ -1,3 +1,4 @@
 import os
-os.system("git bisect start c1a4be04b972b6c17db242fc37752ad517c29402 e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c")
+import sys
+os.system("git bisect start " +sys.argv[1] + " e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c")
 os.system("git bisect run python3.9 manage.py test")
